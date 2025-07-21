3b58cde9641d24f9a0db0467804622f6faec636d
Hacker-Junior <juniorhackerno.1@gmail.com>
2025-07-21 08:37:34 +0300

    Update id.js

--- id.js
+++ id.js
@@ -3,7 +3,7 @@ function makeid(num = 4) {
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
-    result += NIGHTRIDDERcharacters.charAt(Math.floor(Math.random() * characters9));
+    result += characters.charAt(Math.floor(Math.random() * characters9));
   }
   return result;
 }
